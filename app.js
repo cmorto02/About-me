ab63c2b8f96f0627c9b9d84e725ef635f49ccf91
cmorto02 <chrismorton.cm@gmail.com>
2019-01-08 17:34:17 -0800

    added questions to .js and chnged a few things in .html

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
 'use strict';
 
+var name = prompt('what is your name?');
+console.log('Visitor: ', name);
 
-var firstanswer = prompt('Is the sky blue?').toUpperCase();
-console.log('Do they think the sky is blue? ', firstanswer);
+alert('Hi, ' + name + ' we are going to play a guessing game.');
+
+alert('The questions are about me. Please answer them with a yes or no.');
+
+var firstanswer = prompt('Have I ever jumped out of a plane?').toUpperCase();
+console.log('Do they think I\'ve jumped out of a plane? ', firstanswer);
 
 if (firstanswer === 'YES' || firstanswer === 'Y') {
   alert('CORRECT!');
 } else {
-  alert('Are you sure?');
+  alert('I have!');
+}
+
+var secondanswer = prompt('Have I ever crashed a humvee?').toUpperCase();
+console.log('Do they think I\'ve crashed a humvee? ', secondanswer);
+
+if (secondanswer === 'YES' || secondanswer === 'Y') {
+  alert('Nope!');
+} else {
+  alert('You are correct!');
+}
+
+var thirdanswer = prompt('Have I been to more than 20 countries?').toUpperCase();
+console.log('Do they think I\'ve been to more than 20 countries? ', thirdanswer);
+
+if (thirdanswer === 'YES' || thirdanswer === 'Y') {
+  alert('CORRECT!');
+} else {
+  alert('I have!');
+}
+
+var fourthanswer = prompt('Have I ever been spearfishing?').toUpperCase();
+console.log('Do they think I\'ve been spearfishing? ', fourthanswer);
+
+if (fourthanswer === 'YES' || fourthanswer === 'Y') {
+  alert('CORRECT!');
+} else {
+  alert('I have!');
+}
+
+var fifthanswer = prompt('Do I like crab?').toUpperCase();
+console.log('Do they think I like crab ', fifthanswer);
+
+if (fifthanswer === 'YES' || fifthanswer === 'Y') {
+  alert('Wrong!');
+} else {
+  alert('NO SEA BUGS!');
 }
