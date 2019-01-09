388d8746ca5df3b8f2396f142ad11b397eb313e9
cmorto02 <chrismorton.cm@gmail.com>
2019-01-08 18:22:23 -0800

    Finishing up

--- app.js
+++ app.js
@@ -11,43 +11,43 @@ var firstanswer = prompt('Have I ever jumped out of a plane?').toUpperCase();
 console.log('Do they think I\'ve jumped out of a plane? ', firstanswer);
 
 if (firstanswer === 'YES' || firstanswer === 'Y') {
-  alert('CORRECT!');
+  document.getElementById('one').innerHTML = ('CORRECT!');
 } else {
-  alert('I have!');
+  document.getElementById('one').innerHTML = ('Wrong! I have jumped out of a plane.');
 }
 
 var secondanswer = prompt('Have I ever crashed a humvee?').toUpperCase();
 console.log('Do they think I\'ve crashed a humvee? ', secondanswer);
 
-if (secondanswer === 'YES' || secondanswer === 'Y') {
-  alert('Nope!');
+if (secondanswer === 'NO' || secondanswer === 'N') {
+  document.getElementById('two').innerHTML = ('CORRECT!');
 } else {
-  alert('You are correct!');
+  document.getElementById('two').innerHTML = ('NOPE! I\'ve high-centered one, but i havent crashed one.');
 }
 
 var thirdanswer = prompt('Have I been to more than 20 countries?').toUpperCase();
 console.log('Do they think I\'ve been to more than 20 countries? ', thirdanswer);
 
 if (thirdanswer === 'YES' || thirdanswer === 'Y') {
-  alert('CORRECT!');
+  document.getElementById('three').innerHTML = ('CORRECT!');
 } else {
-  alert('I have!');
+  document.getElementById('three').innerHTML = ('Wrong, I have!');
 }
 
 var fourthanswer = prompt('Have I ever been spearfishing?').toUpperCase();
 console.log('Do they think I\'ve been spearfishing? ', fourthanswer);
 
 if (fourthanswer === 'YES' || fourthanswer === 'Y') {
-  alert('CORRECT!');
+  document.getElementById('four').innerHTML = ('CORRECT!');
 } else {
-  alert('I have!');
+  document.getElementById('four').innerHTML = ('Wrong, I have!');
 }
 
 var fifthanswer = prompt('Do I like crab?').toUpperCase();
 console.log('Do they think I like crab ', fifthanswer);
 
-if (fifthanswer === 'YES' || fifthanswer === 'Y') {
-  alert('Wrong!');
+if (fifthanswer === 'NO' || fifthanswer === 'N') {
+  document.getElementById('five').innerHTML = ('CORRECT! Sea bugs are horrible!');
 } else {
-  alert('NO SEA BUGS!');
+  document.getElementById('five').innerHTML = ('Nope, I hate them.');
 }
